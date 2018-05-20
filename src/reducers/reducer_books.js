5d738e56b3ab8eb11989c78bc18d642e272ab7fd
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-20 15:16:50 +0700

    declare reducers data

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { 'title' : 'javascript is good path' },
+    { 'title' : 'harry potter' },
+    { 'title' : 'good javascript' },
+    { 'title' : 'OMG this react' },
+  ]
+}
